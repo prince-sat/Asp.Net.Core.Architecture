40bf4de5ee9e63239b89520acff56796b0c07961
EXT_HBE57 <EXT_HBE57@PAR1DSK0319>
2018-04-16 16:40:47 +0200

    ajout bibliotheque Ressource

---
+++ Asp.Net.Core.Architecture/Asp.Net.Core.Ressources/Shared/SharedResources.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.Net.Core.Ressources.Shared
+{
+    public static class SharedResources
+    {
+        public const string DATE_CREATION = "Date de création";
+        public const string DATE_MODIFICATION = "Date de modification";
+        public const string DATE_SUPPRESSION = "Date de suppression";
+        public const string FONCTIONNALITE_CREATION = "Créé par la fonctionnalité";
+        public const string FONCTIONNALITE_MODIFICATION = "Modifié par la fonctionnalité";
+        public const string FONCTIONNALITE_SUPPRESSION = "Supprimé par la fonctionnalité";
+        public const string UTILISATEUR_CREATION = "Créé par";
+        public const string UTILISATEUR_MODIFICATION = "Modifié par";
+        public const string UTILISATEUR_SUPPRESSION = "Supprimé par";
+    }
+}
