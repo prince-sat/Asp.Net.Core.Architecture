ce21a0de197fb291a5b05ec4b0fcc8d061d59753
EXT_HBE57 <EXT_HBE57@PAR1DSK0319>
2018-07-23 15:58:29 +0200

    ajout de la couche business layer dans l'architecture

---
+++ Asp.Net.Core.Architecture/Asp.Net.Core.BusinessLayer/Interfaces/IRoleService.cs
@@ -0,0 +1,17 @@
+﻿using Asp.Net.Core.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.Net.Core.BusinessLayer.Interfaces
+{
+    public interface IRoleService
+    {
+        /// <summary>
+        /// Retourne l'objet role
+        /// </summary>
+        /// <param name="roleId">Identifiant du role</param>
+        /// <returns></returns>
+        Role RolesSelonId(int roleId);
+    }
+}
---
+++ Asp.Net.Core.Architecture/Asp.Net.Core.BusinessLayer/Services/RoleService.cs
@@ -0,0 +1,32 @@
+﻿using Asp.Net.Core.BusinessLayer.Interfaces;
+using Asp.Net.Core.DataAccessLayer.Interface;
+using Asp.Net.Core.Models.Models;
+using Asp.Net.Core.Transverse.Logger.Interface;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.Net.Core.BusinessLayer.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericLogger _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public RoleService(IUnitOfWork unitOfWork,
+          IHttpContextAccessor httpContextAccessor,
+          IGenericLogger<RoleService> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Role RolesSelonId(int roleId)
+        {
+            return _unitOfWork.RoleRepository.GetSingle(roleId);
+        }
+    }
+}
